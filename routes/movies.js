ee4739812c6563fe5e2215fef99ba8aaaf4d848c
Gerard Solanes Hernandez <gerardsolher@gmail.com>
2022-12-22 13:32:16 +0100

    Add detail view for each movie implementing dynamic url params

--- routes/movies.js
+++ routes/movies.js
@@ -2,15 +2,29 @@ const express = require('express');
 const router = express.Router();
 const Movie = require('../models/Movie.model.js');
 
-/* GET home page */
+/* GET all movies page */
+/* ROUTE /movies */
 router.get('/', async (req, res, next) => {
     try {
         const movies = await Movie.find({});
-        // console.log({movies});
         res.render('movies', { movies });
     } catch (error) {
         next(error);
     }
 });
+
+/* GET See more page */
+/* ROUTE /movies/:movieId */
+router.get('/:movieId', async (req, res, next) => {
+    const { movieId } = req.params;
+    try {
+        const movie = await Movie.findById(movieId);
+        // console.log(movie);
+        res.render('more-info', movie);
+    } catch (error) {
+        next(error);
+    }
+})
+
 
 module.exports = router;
